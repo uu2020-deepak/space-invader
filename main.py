c92af54eb395a01f4f953bd13db4afacee849f7b
Aarav Tyagi <uu2020.deepak@gmail.com>
2026-01-18 22:35:58 +0530

    sound and text added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import random
+from pygame import mixer
 pygame.init()
 
 # Screen setup
@@ -14,6 +15,10 @@ pygame.display.set_icon(icon)
 
 # Load background image
 background = pygame.image.load('background.jpg')
+
+#background music
+mixer.music.load('background.wav')
+mixer.music.play(-1)
 
 # Player setup
 player_img = pygame.image.load('arcade-game (1).png')
@@ -42,7 +47,7 @@ bullet_img = pygame.image.load('bullet.png')
 bullet_x = 0
 bullet_y = 480  #starting Y position
 bullet_x_change = 0
-bullet_y_change = 0.5 #vertical speed
+bullet_y_change = 7 #vertical speed
 bullet_state = "ready"
 
 def draw_player(x, y):
@@ -67,8 +72,24 @@ def is_collision(enemy_x, enemy_y, bullet_x, bullet_y):
         return True
     else:
         return False
+
+#score
+score_value=0
+font = pygame.font.Font("greats Race Italic.ttf", 50)
+textX= 10
+textY= 10
+
+#game over text
+over_font = pygame.font.Font("greats Race Italic.ttf", 100)
+
+def show_score(x,y):
+    score = font.render('score=' + str(score_value), True, (255, 255, 255))
+    screen.blit(score, (x,y))
+
+def game_over_font():
+    over_text = over_font.render("GAME OVER !!!", True, (255, 255, 255))
+    screen.blit(over_text, (200, 250))
 # Game loop
-score=0
 running = True
 while running:
     # Draw the background
@@ -83,13 +104,15 @@ while running:
             # When user presses a key
             if event.key == pygame.K_LEFT:
                 # Move player left
-                player_x_change = -0.5
+                player_x_change = -1
             if event.key == pygame.K_RIGHT:
                 # Move player right
-                player_x_change = 0.5
+                player_x_change = 1
             if event.key == pygame.K_SPACE:
                 # move bullet
-                if bullet_state is "ready":
+                if bullet_state == "ready":
+                    bullet_sound= mixer.Sound('laser.wav')
+                    bullet_sound.play()
                     bullet_x = player_x
                     fire_bullet(bullet_x, bullet_y)
         if event.type == pygame.KEYUP:
@@ -113,8 +136,14 @@ while running:
     # Update enemy position and movement
     # Move the enemy horizontally
     for i in range(num_of_enemies):
+        if enemy_y[i] > 440:
+            for j in range(num_of_enemies):
+                enemy_y[j]= 2000
+            game_over_font()
+            break
+
+
         enemy_x[i] += enemy_x_change[i]
-    
     # Clamp enemy position and handle boundary behavior
     # Keep the enemy from going off the left edge
         if enemy_x[i] <= 0:
@@ -132,10 +161,12 @@ while running:
             enemy_y[i] += enemy_y_change[i]
         collision= is_collision(enemy_x[i], enemy_y[i], bullet_x, bullet_y)
         if collision:
+            collision_sound = mixer.Sound('explosion.wav')
+            collision_sound.play()
             bullet_y = 480
             bullet_state = "ready"
-            score += 1  
-            print(score)
+            score_value += 1
+
             enemy_x[i] = random.randint(0, 735)
             enemy_y[i] = random.randint(50, 150)
         draw_enemy(enemy_x[i], enemy_y[i], i)
@@ -148,8 +179,11 @@ while running:
         bullet_y = 480
         bullet_state = "ready"
 
-    if bullet_state is "fire":
+    if bullet_state == "fire":
         fire_bullet(bullet_x, bullet_y)
         bullet_y -= bullet_y_change
     # Update the display to show all drawn objects
+
+    show_score(textX, textY)
+
     pygame.display.update()
